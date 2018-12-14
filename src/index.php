5991891b70a9e1c1acf45fb96ac5de81dd78b800
robert <oranje@mail.com>
2018-12-14 10:49:38 +0100

    housekeeping

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Yellow Heroes
+ * Project: bootwrap
+ * File: index.php
+ * User: Robert
+ * Date: 14/12/2018
+ * Time: 10:21
+ */
+echo 'hello';
+$index = './views/index.php';
+echo $index;
+header('Location: ' . $index);
+exit();
